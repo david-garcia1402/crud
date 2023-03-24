83fd5e49ae334a3b9635c336971ec48abb59cdb0
Valmir F <120138460+david-garcia1402@users.noreply.github.com>
2023-03-24 16:43:55 -0300

    SQL insert usuarios

--- formulariophp/salvar-usuario.php
+++ formulariophp/salvar-usuario.php
@@ -1,7 +1,15 @@
-<?php 
+<?php
+    include('config.php');
     switch($_REQUEST["acao"]){
-        case "cadastrar";
+        case "cadastrar":
         $nome = $_GET["name"];
-        $email = $_GET["email"]
+        $email = $_GET["email"];
+        $data_cadastro = $_GET["date"];
+        $password_ = $_GET["password"];
+    
+    $sql = 'INSERT INTO usuarios (nome, email, data_cadastro, password_)
+            VALUES ($nome, $email, $data_cadastro, $password_)';
+    
+    $res = $conn->query($sql);
     }
 ?>
