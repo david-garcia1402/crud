2db093b4c561ec4cad98d623955942e127aa60b5
Valmir F <120138460+david-garcia1402@users.noreply.github.com>
2023-03-24 13:14:52 -0300

    Passo a passo para criar um db

--- formulariophp/random.php
+++ formulariophp/random.php
@@ -8,6 +8,23 @@
 </head>
 <body>
     <h1>INDEX</h1>
-    
+    <!-- 1 passo = criar banco de dados no php myadmin
+         2 passo = tabelas (id int, nome varchar, email varchar, data_nasc date, senha varchar) 
+         3 passo = criar o arquivo config para definir o banco de dados usado
+         4 passo = $conn = new mysqli(host, user, pass, base); (dentro do arquivo config.php)
+         5 passo = dar um "include("config.php")" dentro do index.php
+         6 passo = colocar um switch($_REQUEST["acao"]) em salvar-usuario.php
+                                        case 'cadastrar':
+                                            $nome = $_GET["nome"]
+                                            $email = $_GET["email"]
+                                            $password = $_GET["password"]
+                                            $data_cad = $_GET["data_cad"] 
+                                                
+                                            $sql = "INSERT INTO usuarios (nome, email, password, data_cad) VALUES 
+                                                    ('{$nome}', '{$email}', '{$password}', '{$data_cad}'  )"
+                                            
+                                            $res = $conn->query($sql)    
+    -->
+
 </body>
 </html>
