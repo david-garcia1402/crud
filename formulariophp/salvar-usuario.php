3b7b798c164a5ab7ffde902907eb334b26f3d9b7
Valmir F <120138460+david-garcia1402@users.noreply.github.com>
2023-03-27 17:15:55 -0300

    Some css and a table create

--- formulariophp/salvar-usuario.php
+++ formulariophp/salvar-usuario.php
@@ -8,30 +8,70 @@
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 <body>
+<style>
 
-<div class="container mt-3">
-  <div class="alert alert-success">
-    <strong>CADASTRADO COM SUCESSO!</strong></a>.
-  </div>
+  body{
+    background-color: #f2f2f2;
+  }
+  .container{
+    padding-left: 100px;
+    padding-top: 10px ;
+  }
+  .btn{
+    display: flex;
+    justify-content: start;
+    
+  }
+</style>
+<form action="index.php">
+<button type="submit" class="btn btn-primary m-3">Voltar</button>
+</form>
+ <div class="container"> 
+  <div class="row">
+        <?php
+            require_once('config.php');
+            switch($_REQUEST["acao"]){
+                case "cadastrar":
+                  $nome = $_GET["name"];
+                  $email = $_GET["email"];
+                  $data_cadastro = $_GET["date"];
+                  $password_ = md5($_GET["password"]);
+                  
+                  $sql2 = "SELECT * FROM usuarios WHERE email = '$email'";
+                  $checksql = mysqli_query($conn, $sql2);
 
-  <?php 
-  ?>
-
+                  if(mysqli_num_rows($checksql) == 0){
+                    $sql = "INSERT INTO usuarios (nome, email, data_cadastro, password)
+                    VALUES ('{$nome}', '{$email}', '{$data_cadastro}', '{$password_}')";
+                    $res = $conn->query($sql);
+                  }
+                  else{
+                    header("Location: email-used.php");
+                  }
+                  default:
+                    $listsql = "SELECT id, nome, email, data_cadastro FROM usuarios";
+                    $listres = $conn->query($listsql); 
+                    
+                    echo '<table>';
+                    echo "<tr>";
+                    echo "<th>ID</th>";
+                    echo "<th>Nome</th>";
+                    echo "<th>E-mail</th>";
+                    echo "<th>Data</th>";
+                    echo "</tr>";
+                    while($row = mysqli_fetch_row($listres)){ 
+                      echo "<tr>";
+                      echo "<td>" . $row[0] . "</td>";
+                      echo "<td>" . $row[1] . "</td>";
+                      echo "<td>" . $row[2] . "</td>";
+                      echo "<td>" . $row[3] . "</td>";
+                      echo "</tr>";
+                    }
+                    echo "</table>";
+                  
+            }
+        ?>
+    </div>
+ </div>
 </body>
 </html>
-
-<?php
-    include('config.php');
-    switch($_REQUEST["acao"]){
-        case "cadastrar":
-        $nome = $_GET["name"];
-        $email = $_GET["email"];
-        $data_cadastro = $_GET["date"];
-        $password_ = md5($_GET["password"]);
-    
-    $sql = "INSERT INTO usuarios (nome, email, data_cadastro, password)
-            VALUES ('{$nome}', '{$email}', '{$data_cadastro}', '{$password_}')";
-    
-    $res = $conn->query($sql);
-    }
-?>
