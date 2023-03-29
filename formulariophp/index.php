11a22f02e1a24738a8f07fd92146e737c698f2bd
Valmir F <120138460+david-garcia1402@users.noreply.github.com>
2023-03-29 16:27:26 -0300

    Update index.php

--- formulariophp/index.php
+++ formulariophp/index.php
@@ -43,29 +43,29 @@
     </style>
 <form action="salvar-usuario.php" method="GET" class="was-validated">        
   <input type="hidden" name ="acao" value="cadastrar">
-    <div class = "login">
-      <div class ="container">
-        <div class ="row">
-          <div class="card">
-            <div class="mb-3 mt-3">  
-              <label for="name" class="form-label">Nome:</label>
-              <input type="text" class="form-control" id="name" name="name" placeholder="Insira seu nome" required>
+      <div class = "login">
+        <div class ="container">
+          <div class ="row">
+            <div class="card">
+              <div class="mb-3 mt-3">  
+                <label for="name" class="form-label">Nome:</label>
+                <input type="text" class="form-control" id="name" name="name" placeholder="Insira seu nome" required>
+              </div>
+              <div class="mb-3">
+                <label for="email" class="form-label">E-mail:</label>
+                <input type="email" id="email" name="email" class="form-control" placeholder = "Insira seu e-mail"required>
+              </div>
+              <div class="mb-3">
+                <label for="datetime" class="form-date">Data de cadastro </label>
+                <input type="date" id ="date" name="date" class="form-control" required>
+              </div>
+                <label for="password">Senha:</label>
+                <input type="password" id = "password" name="password" class="form-control mb-3" required>
+                <button type="submit">Cadastrar</button>
             </div>
-            <div class="mb-3">
-              <label for="email" class="form-label">E-mail:</label>
-              <input type="email" id="email" name="email" class="form-control" placeholder = "Insira seu e-mail"required>
-            </div>
-            <div class="mb-3">
-              <label for="datetime" class="form-date">Data de cadastro </label>
-              <input type="date" id ="date" name="date" class="form-control" required>
-            </div>
-              <label for="password">Senha:</label>
-              <input type="password" id = "password" name="password" class="form-control mb-3" required>
-              <button type="submit">Cadastrar</button>
           </div>
         </div>
       </div>
-    </div>
 </form> 
 </body>
 <?php 
