20929b4b9969209ac32e2a4660172855083a0a7b
Valmir F <120138460+david-garcia1402@users.noreply.github.com>
2023-04-03 14:42:50 -0300

    Adicionando o bootstrap versão anterior (4.6)

--- formulariophp/index.php
+++ formulariophp/index.php
@@ -7,6 +7,11 @@
     <!-- Bootstrap link/script para auxílio do front-end -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">  
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="funcexcluir.js"></script>
     <title>Teste</title>
 </head>
 <body>
