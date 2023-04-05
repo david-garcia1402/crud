d83c565964c416fa78d50850e10a2a550ff9de0c
Valmir F <120138460+david-garcia1402@users.noreply.github.com>
2023-04-05 17:03:52 -0300

    Update salvar-usuario.php

--- formulariophp/salvar-usuario.php
+++ formulariophp/salvar-usuario.php
@@ -17,10 +17,7 @@
             $res = $conn->query($sql);
           }
           else{
-            echo '<div class="alert alert-info">' +
-            '<strong>Atenção!</strong> Não tem nenhum usuário listado neste momento.' +
-            '<div class="button mx-auto">'+
-            '</div>';
+            header("Location: email-used.php"); //se já houver um e-mail igual registrado, vai ser mandado para o arquivo 'email-used.php' alertando que o e-mail já está sendo usado
           } 
     }
 ?>
