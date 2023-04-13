d47aed4ebcefed79a35bc9df1f0bf8a03a2c3edc
Valmir F <120138460+david-garcia1402@users.noreply.github.com>
2023-04-13 17:13:48 -0300

    Update excluido-user.php

--- formulariophp/excluido-user.php
+++ formulariophp/excluido-user.php
@@ -3,6 +3,4 @@
      $excluir = $_GET["excluir"]; 
      $deletesql = "DELETE FROM usuarios WHERE id = '$excluir'"; //query para deletar o usuário do banco de dados através do ID
      $delres = $conn->query($deletesql);
-
-     echo "$excluir"
 ?>
